9dcd88e797803c7954ff068bd32d78186923f17c
mndstrmr <27809483+mndstrmr@users.noreply.github.com>
2023-11-29 15:00:18 +0000

    Add test case from a dice ctf

--- ir/src/clean.rs
+++ ir/src/clean.rs
@@ -101,8 +101,6 @@ pub fn reduce_cmp(code: &mut Vec<Instr>) {
     for instr in code {
         instr.visit_top_exprs_mut(&mut |expr| {
             expr.visit_mut_post(&mut |e| if let Expr::MonOp(m, box Expr::BinOp(c, l, r)) = e {
-                println!("{:?} {:?}", m, c);
-
                 match (*m, *c) {
                     (MonOp::CmpEq, BinOp::And) if *l == *r && !l.might_side_effect() => *e = Expr::BinOp(BinOp::Eq, Box::new(l.take()), Box::new(Expr::Lit(0, Typ::N64))),
                     (MonOp::CmpNe, BinOp::And) if *l == *r && !l.might_side_effect() => *e = Expr::BinOp(BinOp::Ne, Box::new(l.take()), Box::new(Expr::Lit(0, Typ::N64))),
